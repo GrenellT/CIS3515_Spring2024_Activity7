44653e3c41861c1f4d0fcac4b5049f3b12611309
Grenell <TTGrenell@gmail.com>
2024-02-22 11:51:22 -0500

    Passed selectedNumber back to DisplayActivity and changed lyrics textSize

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class DisplayActivity : AppCompatActivity() {
 
-    // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
-
-    // TODO Step 3: Use returned value for lyricsDisplayTextView text size
-
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
+
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == 123) {
+            it.data?.apply {
+                lyricsDisplayTextView.textSize = getIntExtra("selectedNumber", 0).toFloat()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +29,7 @@ class DisplayActivity : AppCompatActivity() {
 
         textSizeSelectorButton.setOnClickListener {
             val launchIntent = Intent(this@DisplayActivity, TextSizeActivity::class.java)
-            startActivity(launchIntent)
+            launcher.launch(launchIntent)
         }
     }
 }
--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -22,9 +23,9 @@ class TextSizeActivity : AppCompatActivity() {
 
         with (findViewById(R.id.textSizeSelectorRecyclerView) as RecyclerView) {
 
-            // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
-            adapter = TextSizeAdapter(textSizes){
-
+            adapter = TextSizeAdapter(textSizes){ selectedTextSize ->
+                setResult(123, Intent().putExtra("selectedNumber", selectedTextSize))
+                finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
